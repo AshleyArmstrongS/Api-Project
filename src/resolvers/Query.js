332b98a6b8c34b7f9bc5a62c23dbe6b6986528f7
AshleyArmstrongS <ashleysarmstrong@gmail.com>
2020-11-22 15:01:37 +0000

    Add the functionality to allow for search Allow for the searching of a single animal or herd

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -3,11 +3,17 @@ var Animal = require("../models/animals");
 function info(){
     return "Hi this is the OptiFarm API."
 }
-function animal(){
-   return Animal.find({})
+function animal(parent, args){
+    console.log(args)
+    const id = args.id
+   return Animal.findById(id)
+}
+function herd(){
+    return Animal.find({});
 }
 
 module.exports = {
     info,
     animal,
+    herd
 }
