ec50c302b9f2c774d14d79cc6b9b0c96b8b65ff2
AshleyArmstrongS <ashleysarmstrong@gmail.com>
2020-11-23 20:50:42 +0000

    Add context and utils file
    add context to pass authorization field
    and add utils to process that field

--- src/index.js
+++ src/index.js
@@ -11,5 +11,10 @@ const resolvers = {
 const server = new GraphQLServer({
     typeDefs: 'src/schema.graphql',
     resolvers,
+    context: request => {
+      return {
+        ...request,
+      }
+    },
   })
 server.start(() => console.log(`Server is running on http://localhost:4000`))
