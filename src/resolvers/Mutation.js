d8035d68cf438496ed427efcac63fed7c6de5d92
AshleyArmstrongS <ashleysarmstrong@gmail.com>
2020-11-25 20:55:12 +0000

    Add more queries and mutations based on animal
    continue to provide queries and mutations for frontend
    
    deleteAnimal
    findBySex
    findByBreed
    findByPureBreed
    findById&Tag

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,11 +1,10 @@
-
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const { APP_SECRET, getUserId } = require('../utils')
 const Animal = require("../models/animals")
 const Farmer = require("../models/farmer")
 
-    async function signup(parent, args, context, info) {
+    async function signUp(parent, args, context, info) {
         const password = await bcrypt.hash(args.password, 10)
         const newFarmer = await new Farmer({
             first_name: args.first_name,
@@ -38,27 +37,30 @@ const Farmer = require("../models/farmer")
         return AuthPayLoad
       }
       async function createAnimal(parent, args){
-        console.log(args)
         const newAnimal = new Animal({
-            tag_number: args.tag_number,
-            herd_number: args.herd_number,
-            sire_number: args.sire_number,
-            mother_number: args.mother_number,
-            male_female: args.male_female,
-            breed_type: args.breed_type,
-            //date_of_birth: args.date_of_birth,
-            pure_breed: args.pure_breed,
-            animal_name: args.animal_name,
-            description: args.description,
-            farmer_id: args.farmer_id
+            tag_number:     args.tag_number,
+            herd_number:    args.herd_number,
+            sire_number:    args.sire_number,
+            mother_number:  args.mother_number,
+            male_female:    args.male_female,
+            breed_type:     args.breed_type,
+            date_of_birth:  args.date_of_birth,
+            pure_breed:     args.pure_breed,
+            animal_name:    args.animal_name,
+            description:    args.description,
+            farmer_id:      args.farmer_id
         })
-        const valid = await newAnimal.save()
+        const error = await newAnimal.save()
         if(error) return error
         return newAnimal
+    }
+    async function deleteAnimal(parent, args){
+      return await Animal.findByIdAndDelete(args.id)
     }
 
 module.exports = {
     createAnimal,
-    signup,
+    signUp,
     login,
+    deleteAnimal,
 }
--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,24 +1,33 @@
 const Animal = require("../models/animals")
 const Farmer = require("../models/farmer")
-const { APP_SECRET, getUserId } = require('../utils')
+const {getUserId } = require('../utils')
 
 function info(){
     return "Hi this is the OptiFarm API."
 }
-function animal(parent, args){
-    var id = args.id
-   return Animal.findById(id)
-}
-function herd(){
-    return Animal.find({});
-}
 function farmer(parent, args, context){
-    return Farmer.findById(getUserId(context))
+    const id = getUserId(context)
+    return Farmer.findById(id)
 }
-
+function animal(parent, args){
+   return Animal.findById(args.id)
+}
+function animalTag(parent, args){
+    return Animal.findOne({"tag_number": args.tag_number})
+}
+async function herd(parent, args, context){
+    const user = await farmer(parent, args, context)
+    var animals = await Animal.find({"herd_number": user.herd_number})
+    return  animals
+}
 module.exports = {
+    //API info
     info,
-    animal,
-    herd,
+    //User
     farmer,
+    //Animal
+    animal,
+    animalTag,
+    //Animals
+    herd,
 }
