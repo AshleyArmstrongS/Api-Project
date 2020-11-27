46609c7287eeede802c60f397a4509584a24609a
Conor Clarke <conorjamesclarke@gmail.com>
2020-11-27 19:03:36 +0000

    Deleted white space

---
+++ src/models/breed.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+mongoose.set("useCreateIndex", true);
+const Schema = mongoose.Schema;
+
+const BreedSchema = new Schema({
+  breed_name: {
+    type: String,
+    required: true
+  },
+  breed_code: {
+    type: String,
+    required: true
+  }
+})
+
+module.exports = mongoose.model('Breed', BreedSchema);
