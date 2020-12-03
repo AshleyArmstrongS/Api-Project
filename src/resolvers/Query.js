3eb733d526e65d6b1faf795ab2702a30b696dd3e
AshleyArmstrongS <ashleysarmstrong@gmail.com>
2020-12-03 19:16:44 +0000

    Implement the suggested GraphQL naming system
    Implement the naming convention for APIs and GraphQL in particular

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -8,36 +8,37 @@ function info(){
     return "This is the OptiFarm API"
 }
 //User
-function getFarmer(parent, args, context){
+function farmer(parent, args, context){
     const id = getUserId(context)
     return Farmer.findById(id)
 }
 //Animal
-function getAnimal(parent, args){
-   return Animal.findById(args.id)
+function animal(parent, args){
+  const id = getUserId(context)
+   return Animal.findOne({ "id" :args.id, "farmer_id": id})
 }
-function getAnimalByTag(parent, args, context){
+function animal(parent, args, context){
     const id = getUserId(context)
     return Animal.findOne({"tag_number": args.tag_number, "farmer_id": id})
 }
 //Animals
-function getHerd(parent, args, context){
+function herd(parent, args, context){
     const id = getUserId(context)
     return Animal.find({"farmer_id": id })
 }
-function getAnimalByBreed(parent, args, context) {
+function animalByBreed(parent, args, context) {
     const id = getUserId(context)
     return Animal.find({"breed_type": args.breed_type, "farmer_id": id })
 }
-function getAnimalByPureBreed(parent, args, context) {
+function animalByPureBreed(parent, args, context) {
     const id = getUserId(context)
     return Animal.find({"pure_breed": args.pure_breed, "farmer_id": id })
 }
-function getAnimalBySex(parent, args, context) {
+function animalBySex(parent, args, context) {
     const id = getUserId(context)
     return Animal.find({"male_female": args.male_female, "farmer_id": id })
 }
-function getProgeny(parent, args, context){
+function progeny(parent, args, context){
     const id = getUserId(context)
     if(args.male_female == "M"){
         return Animal.find({ "sire_number" : args.tag_number, "farmer_id" : id })
@@ -47,72 +48,72 @@ function getProgeny(parent, args, context){
 
 // Search Queries
 
-function getAnimalsByDateOfBirth(parent, args, context) {
+function animalsBornOn(parent, args, context) {
   const id = getUserId(context)
   return Animal.find({"date_of_birth": new Date(args.date_of_birth), "farmer_id": id })
 }
 
-function getAnimalsByDateBornAfter(parent, args, context) {
+function animalsBornAfter(parent, args, context) {
   const id = getUserId(context)
   return Animal.find({"date_of_birth": {$gte : new Date(args.date_of_birth)} , "farmer_id": id })
 }
 
-function getAnimalsByDateBornBefore(parent, args, context) {
+function animalsBornBefore(parent, args, context) {
   const id = getUserId(context)
   return Animal.find({"date_of_birth": {$lte : new Date(args.date_of_birth)} , "farmer_id": id })
 }
 
-function getAnimalsByDatesBornBetween(parent, args, context) {
+function animalsBornBetween(parent, args, context) {
   const id = getUserId(context)
   return Animal.find({"date_of_birth": {$gte : new Date(args.after), $lte : new Date(args.before)} , "farmer_id": id })
 }
 
 //Group
-function getGroup(parent, args){
+function group(parent, args){
   return Group.findById(args.id)
 }
 // might need to put group_name as unique
-function getGroupByName(parent, args, context){
+function groupByName(parent, args, context){
   const id = getUserId(context)
   return Group.find({"group_name": args.group_name, "farmer_id": id})
 }
-function getGroupByDescription(parent, args, context){
+function groupByDescription(parent, args, context){
   const id = getUserId(context)
   return Group.find({"group_description": args.group_description, "farmer_id": id})
 }
 
 //Medication
-function getMedication(parent, args, context) {
+function medication(parent, args, context) {
     return Medication.findById(args.id)
 }
 //Medications
-function getMedications(parent, args, context) {
+function medications(parent, args, context) {
     const id = getUserId(context)
     return Medication.find({"farmer_id" : id})
 }
 module.exports = {
     info,
-    getFarmer,
+    farmer,
 
     // Animal
-    getAnimal,
-    getAnimalByTag,
-    getHerd,
-    getAnimalByBreed,
-    getAnimalByPureBreed,
-    getAnimalBySex,
-    getProgeny,
-    getAnimalsByDateOfBirth,
-    getAnimalsByDateBornAfter,
-    getAnimalsByDateBornBefore,
-    getAnimalsByDatesBornBetween,
+    animal,
+    //animalByTag,
+    herd,
+    animalByBreed,
+    animalByPureBreed,
+    animalBySex,
+    progeny,
+    animalsBornOn,
+    animalsBornAfter,
+    animalsBornBefore,
+    animalsBornBetween,
 
     // Group
-    getGroup,
-    getGroupByName,
-    getGroupByDescription,
+    group,
+    groupByName,
+    groupByDescription,
 
     // Medication
-    getMedication,
-    getMedications,
+    medication,
+    medications,
 }
