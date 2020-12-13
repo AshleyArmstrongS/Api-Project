45ee943c3ee56512a73ecc66e9684f4e65cb1e19
AshleyArmstrongS <ashleysarmstrong@gmail.com>
2020-12-13 12:28:03 +0000

    wrong file Path

--- src/Config.js
+++
@@ -1,7 +0,0 @@
-//Holds the APP_SECRET DB name and other information that may change between development and production
-const APP_SECRET = "OptiFarmSecret";
-const atlasURI = "mongodb+srv://optiOne:oneOpti@cluster0.ikcii.mongodb.net/OptiFarmTestDb?retryWrites=true&w=majority";
-module.exports = {
-  atlasURI,
-  APP_SECRET,
-};
