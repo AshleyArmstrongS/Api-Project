fe11b13c009eff5c36b3f443b7fb214cc4278dc6
AshleyArmstrongS <ashleysarmstrong@gmail.com>
2021-02-05 20:18:53 +0000

    merge conflict

--- src/models/mongoose_validation.js
+++ src/models/mongoose_validation.js
@@ -1,27 +1,28 @@
-const validateFiveDigitNumber = (tag_number) => {
-  const re = /^d{5}$/;
-  return re.test(tag_number);
-};
-const validateHerdNumber = (herd_number) => {
-  const re = /^[a-zA-Z0-9{10}]$/;
-  return re.test(herd_number);
-};
-const validateAnimalName = (animal_name) => {
-  const re = /^[a-zA-Z0-9_]+( [a-zA-Z0-9_]+)*$/;
-  return re.test(animal_name);
-};
-const validateBreedName = (name) => {
-  const re = /^[a-zA-Z ]{2,30}$/;
-  return re.test(name);
-};
-const validateBreedCode = (breed_code) => {
-  const re = /^[a-zA-Z0-9]{2,4}$/;
-  return re.test(breed_code);
-};
-const validateIsName = (name) => {
-  const re = /^[a-zA-Z ]{2,30}$/;
-  return re.test(name);
-};
+//https://stackoverflow.com/questions/4975644/regular-expression-to-match-exactly-5-digits
+const validateFiveDigitNumber = tag_number => {
+  const re = /\b\d{5}\b/g
+  return re.test(tag_number)
+}
+const validateHerdNumber = herd_number => {
+  const re = /^[a-zA-Z0-9{10,11}]$/
+  return re.test(herd_number)
+}
+const validateAnimalName = animal_name => {
+  const re = /^[a-zA-Z0-9_]+( [a-zA-Z0-9_]+)*$/
+  return re.test(animal_name)
+}
+const validateBreedName = name => {
+  const re = /^[a-zA-Z ]{2,30}$/
+  return re.test(name) 
+}
+const validateBreedCode = breed_code => {
+  const re = /^[a-zA-Z0-9]{2,4}$/
+  return re.test(breed_code)
+}
+const validateIsName = name => {
+  const re = /^[a-zA-Z ]{2,30}$/
+  return re.test(name) 
+}
 // https://stackoverflow.com/questions/18022365/mongoose-validate-email-syntax
 var validateEmail = function (email) {
   var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
