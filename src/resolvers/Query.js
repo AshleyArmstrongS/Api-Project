ee7f160866729b3ac7684cbeca8a93ac1c4b423a
Conor Clarke <conorjamesclarke@gmail.com>
2021-03-31 13:27:33 +0100

    Added 4 medication queries
    medicationsLImitOf4 - only returns max 4 medications
    medicationByType - search for medication by type, e.g. DOSE
    medicationsByRemainingQtyLessThan - returns medications with
    quantity less than input
    medicationsByRemainingQtyGreaterThan - returns medications
    with quantity greater than input

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -370,6 +370,79 @@ async function medications(parent, args, context) {
   }
   return returnable;
 }
+async function medicationsLimitOf4(parent, args, context) {
+  const farmer_id = getUserId(context);
+  var returnable = { responseCheck: FAILED_AUTHENTICATION };
+  if (farmer_id) {
+    const medications = await Medication.find({ farmer_id: farmer_id }).limit(4);
+    if (!medications) {
+      returnable = { responseCheck: OPERATION_FAILED };
+    } else {
+      returnable = {
+        responseCheck: OPERATION_SUCCESSFUL,
+        medications: medications,
+      };
+    }
+  }
+  return returnable;
+}
+async function medicationsByType(parent, args, context) {
+  const farmer_id = getUserId(context);
+  var returnable = { responseCheck: FAILED_AUTHENTICATION };
+  if (farmer_id) {
+    const medications = await Medication.find({ 
+      medicine_type: args.medicine_type,
+      farmer_id: farmer_id 
+    });
+    if (!medications) {
+      returnable = { responseCheck: OPERATION_FAILED };
+    } else {
+      returnable = {
+        responseCheck: OPERATION_SUCCESSFUL,
+        medications: medications,
+      };
+    }
+  }
+  return returnable;
+}
+async function medicationsByRemainingQtyLessThan(parent, args, context) {
+  const farmer_id = getUserId(context);
+  var returnable = { responseCheck: FAILED_AUTHENTICATION };
+  if (farmer_id) {
+    const medications = await Medication.find({ 
+      remaining_quantity: { $lte: args.remaining_quantity},
+      farmer_id: farmer_id 
+    });
+    if (!medications) {
+      returnable = { responseCheck: OPERATION_FAILED };
+    } else {
+      returnable = {
+        responseCheck: OPERATION_SUCCESSFUL,
+        medications: medications,
+      };
+    }
+  }
+  return returnable;
+}
+async function medicationsByRemainingQtyGreaterThan(parent, args, context) {
+  const farmer_id = getUserId(context);
+  var returnable = { responseCheck: FAILED_AUTHENTICATION };
+  if (farmer_id) {
+    const medications = await Medication.find({ 
+      remaining_quantity: { $gte: args.remaining_quantity},
+      farmer_id: farmer_id 
+    });
+    if (!medications) {
+      returnable = { responseCheck: OPERATION_FAILED };
+    } else {
+      returnable = {
+        responseCheck: OPERATION_SUCCESSFUL,
+        medications: medications,
+      };
+    }
+  }
+  return returnable;
+}
 async function medicationsExpired(parent, args, context) {
   const farmer_id = getUserId(context);
   var returnable = { responseCheck: FAILED_AUTHENTICATION };
@@ -546,6 +619,10 @@ module.exports = {
   // Medication
   medication,
   medications,
+  medicationsLimitOf4,
+  medicationsByType,
+  medicationsByRemainingQtyLessThan,
+  medicationsByRemainingQtyGreaterThan,
   medicationsExpired,
   medicationsByName,
   // AdministeredMedication
