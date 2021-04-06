c8523e05a451b7b23c29118ee1e7c8bb3ccfca5d
AshleyArmstrongS <ashleysarmstrong@gmail.com>
2021-04-06 15:30:39 +0100

    remove bearer token bypass bypass used for testing a frontend issue

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 const { APP_SECRET } = require("./Config");
 function getUserId(context) {
-  const Authorization = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI1ZmJkNWE2NGJjOTM3MDA0ZjRhZDIxZDIiLCJpYXQiOjE2MTU4Mzc4NTF9.Xs_3dzxP6nwzReyaeBRbjQqOxWtykfuWgMxoMzQdylA"//context.request.get("Authorization");
+  const Authorization = context.request.get("Authorization");
   if (Authorization) {
     const token = Authorization.replace("Bearer ", "");
     try {
