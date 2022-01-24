6cabba7170110c621f5fb04eb7e50f6b4e2fabc4
AshleyArmstrongS <ashleysarmstrong@gmail.com>
2022-01-24 16:29:42 +0000

    Move admin med to own file and finish
    finishes moving queries to separate files

--- src/resolvers/Queries/User.js
+++ src/resolvers/Queries/User.js
@@ -1,24 +1,29 @@
 const User = require("../../models/user");
 const { getUserId } = require("../utils");
+const {
+  FAILED_AUTHENTICATION,
+  OPERATION_SUCCESSFUL,
+  OPERATION_FAILED,
+} = require("./ResolverErrorMessages");
 //API info
 function info() {
-    return "This is the OptiFarm API";
-  }
-  //User
-  async function user(parent, args, context) {
-    const user_id = getUserId(context);
-    var returnable = { responseCheck: FAILED_AUTHENTICATION };
-    if (user_id) {
-      const user = await User.findById(user_id);
-      if (!user) {
-        returnable = { responseCheck: OPERATION_FAILED };
-      } else {
-        returnable = { responseCheck: OPERATION_SUCCESSFUL, user: user };
-      }
+  return "This is the OptiFarm API";
+}
+//User
+async function user(parent, args, context) {
+  const user_id = getUserId(context);
+  var returnable = { responseCheck: FAILED_AUTHENTICATION };
+  if (user_id) {
+    const user = await User.findById(user_id);
+    if (!user) {
+      returnable = { responseCheck: OPERATION_FAILED };
+    } else {
+      returnable = { responseCheck: OPERATION_SUCCESSFUL, user: user };
     }
-    return returnable;
   }
-  module.exports = {
-    info,
-    user,
-  };
+  return returnable;
+}
+module.exports = {
+  info,
+  user,
+};
